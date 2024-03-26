f1c393b65825a3db4368da0b481463aaf9cc7861
Khuldoon171 <164254981+Khuldoon171@users.noreply.github.com>
2024-03-27 01:17:32 +0500

    Add files via upload

---
+++ park.js
@@ -0,0 +1,19 @@
+//Park Entry System
+var user = "childern";
+var user1 = "adult";
+var user2 = "senior";
+var entryFee = 200;
+var entryFee1 = 800;
+var entryFee2 = 500;
+if (user == "childern") {
+    console.log(user, entryFee);
+}
+if (user1 == "adult") {
+    console.log(user1, entryFee1);
+}
+if (user2 = "senior") {
+    console.log(user2, entryFee2);
+}
+else {
+    console.log("invaild");
+}
---
+++ park.ts
@@ -0,0 +1,19 @@
+//Park Entry System
+let user = "childern";
+let user1 = "adult";
+let user2 = "senior";
+const entryFee  = 200;
+const entryFee1 = 800;
+const entryFee2 = 500;
+
+
+if(user == "childern"){
+console.log(user, entryFee);
+}
+if(user1 == "adult"){
+    console.log(user1, entryFee1);
+}
+if(user2 = "senior"){
+    console.log(user2, entryFee2)
+}
+else{console.log("invaild")}
